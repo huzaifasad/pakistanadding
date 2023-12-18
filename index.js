d8cba6a6f729b9e1814056a1413e7c678a48187f
Huzaifa Saad <140094374+huzaifasad@users.noreply.github.com>
2023-12-18 11:30:38 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import multer from 'multer';
-import { Laptop } from './schemas/productschema/Laptop.js'; // Import using the exact same name
+import { Laptop } from './schemas/productschema/Laptop.js';
 import Signup from './schemas/user.js';
-
 
 const app = express();
 const upload = multer();
@@ -21,17 +20,18 @@ app.listen(5000);
 app.use(
   cors({
     origin: [''],
-    methods: ['POST', 'GET', 'PUT', 'DELETE'], // Include PUT and DELETE methods
+    methods: ['POST', 'GET', 'PUT', 'DELETE'],
     credentials: true,
   })
 );
- 
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
   res.json('ALLAH HU AKBAR');
 });
 
-app.use(bodyParser.json({ extended: true }));
-app.use(bodyParser.urlencoded({ extended: true }));
 app.post('/laptop/add', async (req, res) => {
   const { name, price, ramSize, type, brand, features, images } = req.body;
 
@@ -41,7 +41,7 @@ app.post('/laptop/add', async (req, res) => {
     ramSize,
     type,
     brand,
-    images, // Assuming images is an array of image URLs
+    images,
     features,
   });
 
@@ -54,13 +54,9 @@ app.post('/laptop/add', async (req, res) => {
   }
 });
 
- 
 app.get('/laptop/get', async (req, res) => {
   try {
     const laptops = await Laptop.find();
-
-    
-
     res.send(laptops);
   } catch (error) {
     console.error('Error fetching laptops:', error);
@@ -82,80 +78,68 @@ app.get('/update/:id', async (req, res) => {
   }
 });
 
-app.put('/update/:id', async (req, res) => {
-  let data = await Laptop.updateOne(
-    { _id: req.params.id },
-    {
-      $set: req.body,
+// Fix routes by adding a slash at the beginning
+app.post('/user/signup', async (req, res) => {
+  try {
+    const existingUser = await Signup.findOne({ email: req.body.email });
+
+    if (existingUser) {
+      return res.status(409).json({ message: 'Account with this email already exists' });
     }
-  );
-  res.send(data);
-  console.log(data);
+
+    let data = new Signup(req.body);
+    let result = await data.save();
+    result = result.toObject();
+    delete result.password;
+
+    res.status(201).json(result);
+  } catch (error) {
+    console.error('Error during signup:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
 });
-app.post('user/signup', async (req, res) => {
-    try {
-      const existingUser = await Signup.findOne({ email: req.body.email });
-  
-      if (existingUser) {
-        return res.status(409).json({ message: 'Account with this email already exists' });
-      }
-  
-      let data = new signup(req.body); 
-      let result = await data.save();
-      result = result.toObject();
-      delete result.password;
-  
-      res.status(201).json(result); 
-    } catch (error) {
-      console.error('Error during signup:', error);
-      res.status(500).json({ message: 'Internal Server Error' });
-    }
-  });
-  
-   
-    app.post('user/login', async (req, res) => {
-      try {
-        const existingUser = await Signup.findOne({ _id: req.body.id });
-  
-         
-      } catch (error) {
-        console.error('Error during login:', error);
-        res.status(500).json({ message: 'Internal Server Error' });
-      }
-    });
-  
-  
-    app.put('user/update-account/:userId', async (req, res) => {
-      try {
-        const user = await Signup.findByIdAndUpdate(req.params.userId, req.body, { new: true });
-    
-        if (user) {
-          const sanitizedUser = { ...user.toObject(), password: undefined };
-          res.json(sanitizedUser);
-        } else {
-          res.status(404).json({ message: 'User not found' });
-        }
-      } catch (error) {
-        console.error('Error during account update:', error);
-        res.status(500).json({ message: 'Internal Server Error' });
-      }
-    });
-    
-  app.delete('user/delete-account', async (req, res) => {
-    try {
-      const existingUser = await Signup.findOne({ email: req.body.email });
-      console.log(req.body.email)
-      if (existingUser) {
-        await signup.deleteOne({ email: req.body.email });
-        res.json({ message: 'Account deleted successfully' });
-      } else {
-        res.status(404).json({ message: 'User not found' });
-      }
-    } catch (error) {
-      console.error('Error during account deletion:', error);
-      res.status(500).json({ message: 'Internal Server Error' });
-    }
-  });
 
+// Add a slash at the beginning
+app.post('/user/login', async (req, res) => {
+  try {
+    const existingUser = await Signup.findOne({ _id: req.body.id });
+    // ...
+  } catch (error) {
+    console.error('Error during login:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
 
- 
+// Add a slash at the beginning
+app.put('/user/update-account/:userId', async (req, res) => {
+  try {
+    const user = await Signup.findByIdAndUpdate(req.params.userId, req.body, { new: true });
+
+    if (user) {
+      const sanitizedUser = { ...user.toObject(), password: undefined };
+      res.json(sanitizedUser);
+    } else {
+      res.status(404).json({ message: 'User not found' });
+    }
+  } catch (error) {
+    console.error('Error during account update:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+// Add a slash at the beginning
+app.delete('/user/delete-account', async (req, res) => {
+  try {
+    const existingUser = await Signup.findOne({ email: req.body.email });
+
+    if (existingUser) {
+      await Signup.deleteOne({ email: req.body.email });
+      res.json({ message: 'Account deleted successfully' });
+    } else {
+      res.status(404).json({ message: 'User not found' });
+    }
+  } catch (error) {
+    console.error('Error during account deletion:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
